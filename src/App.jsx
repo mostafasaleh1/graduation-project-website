34a3ccb5a3ee89a0df9375320873d6aa3a0d6c0e
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-05-07 21:53:23 +0300

    creating the navbar and menu icon on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import styles from "./styles";
+import Navbar from "./components/Navbar";
+import Stats from "./components/Stats";
+import Footer from './components/Footer';
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Footer
+          <Stats />
+          <Footer />
         </div>
       </div>
 
