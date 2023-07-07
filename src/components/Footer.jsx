9df26c044e916bc5b6cb8e60e8b81f803fa64ad6
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-07-07 15:47:59 +0300

    creating favicon for the website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => (
             <div className="flex-[1] flex flex-col justify-start mr-10">
                 <img
                     src={logo}
-                    alt="hoobank"
+                    alt="Intelligent Mobile Robot"
                     className="w-[266px] h-[72.14px] object-contain"
                 />
                 <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false)
     return (
         <nav className='w-full flex py-6 justify-between items-center navbar'>
-            <img src={logo} alt="hoobank"
+            <img src={logo} alt="Intelligent Mobile Robot"
                 className='w-[124px] h-[32px]' />
 
             <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
