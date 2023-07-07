4a98fbc127ccede1739b315a8f7c303c52d676ec
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-07-07 18:11:08 +0300

    adding team photo to contributers section.

--- src/components/Contributers.jsx
+++ src/components/Contributers.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import styles, { layout } from "../styles";
+import Footer from './Footer';
+import Button from './Button';
+import { ourTeam } from '../assets';
 
 const Contributers = () => {
     return (
-        <div>Contributers</div>
+        <section id="features" className={layout.sectionCol}>
+            <img src={ourTeam} alt="Contributers" className="rounded-3xl" />
+            <Footer />
+        </section>
     )
 }
 
