c745ef14e907bc0e1234d8a9656037d9c887435e
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-07-09 18:15:03 +0300

    updating presentation and book links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,13 +28,13 @@ const About = () => {
             </div>
 
             <div id="Description" className={activeTab ? `hidden ${styles.flexCenter} flex flex-col` : `block ${styles.flexCenter} flex flex-col`}>
-                <iframe src="https://onedrive.live.com/embed?resid=317E50C5E6FB11CA%21352&authkey=!AMz45dAOsWNpKQ4&em=2" className='w-[1333px] h-[750px]' frameborder="0" scrolling="no"></iframe>
-                <Button link="https://public.bn.files.1drv.com/y4mwO7a-O8MA_Jf8NddCVN3_B69Y_PKpBpu2l_lqg-jXIQMUmaRZXlUWuqQ5tQUpICjSyDjPZnGNdEOS2v8wJLi5aW4-beiQzoLXGmAenZihYdouVqZ95-UxULKoYJY10dKrK_--PqFAeOmDAoDMIAZILI-7evmuzSmZJZEI8PTM3kTRwCPKuv-dkICuCjDRPccmiu_09Ysxpw5fNycTZVwxC6KhQjHAQE5J02-c9BCeu4?AVOverride=1" content="Download Presentation" />
+                <iframe src="https://onedrive.live.com/embed?resid=317E50C5E6FB11CA%21355&authkey=!AGBRAYQDvqN2wc8&em=2" className='w-[1333px] h-[750px]' frameborder="0" scrolling="no"></iframe>
+                <Button link="https://raw.githubusercontent.com/TheHero100/Intelligent-Mobile-Robot-IMR/main/Documentations/v2/IMR%20presentaion%20final.pdf" content="Download Presentation" />
             </div>
 
             <div id="Specifications" className={activeTab ? `block ${styles.flexCenter} flex flex-col` : `hidden ${styles.flexCenter} flex flex-col`}>
                 <iframe src="https://onedrive.live.com/embed?resid=317E50C5E6FB11CA%21353&authkey=!AFs9VS72l3EKxVE&em=2" className='w-[793px] h-[1122px]' frameborder="0" scrolling="no"></iframe>
-                <Button link="https://public.bn.files.1drv.com/y4mSfSkr-fdiRgRBsd1DkGheZqCDSPDn9YHiWl5q8LFhjS42yT4iSZxXubr3ECLM4LFLRjMhDLmeJQi942rpHhoOEAWHq22MAfu5YR-jfv2MPks7n0KHe0RpkNQOSTCgmjHO0AiPNSTDWIKJjwtRkcXiZe-PG4zu_IEhL5GL0zZNCgOga8TCo3WJUuvak3TMWScsOuj1eW1hG6YrfXES7JYwYsa1cwhi9-ImOE4_jadleA?AVOverride=1" content="Download Book" />
+                <Button link="https://raw.githubusercontent.com/TheHero100/Intelligent-Mobile-Robot-IMR/bb0b1829b285ddb869d8a41f85ae3a66b9649c72/Documentations/v2/IMR%20Final%20Book.pdf" content="Download Book" />
             </div>
 
             <div className='mt-[10rem]'>
