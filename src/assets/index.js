74b6fc235143ca77dc94461b0f520eda0d833fca
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-07-11 01:09:56 +0300

    changing credits and replacing social media links with only github

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,10 +4,7 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import arrowUp from "./arrow-up.svg";
 import discount from "./Discount.svg";
-import facebook from "./facebook.svg";
-import instagram from "./instagram.svg";
-import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
+import github from "./github.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
@@ -57,10 +54,7 @@ export {
   close,
   arrowUp,
   discount,
-  facebook,
-  instagram,
-  linkedin,
-  twitter,
+  github,
   people01,
   people02,
   people03,
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,10 +2,7 @@ import {
   people01,
   people02,
   people03,
-  facebook,
-  instagram,
-  linkedin,
-  twitter,
+  github,
 } from "../assets";
 
 export const navLinks = [
@@ -95,22 +92,7 @@ export const footerLinks = [
 export const socialMedia = [
   {
     id: "social-media-1",
-    icon: instagram,
-    link: "https://www.instagram.com/",
-  },
-  {
-    id: "social-media-2",
-    icon: facebook,
-    link: "https://www.facebook.com/",
-  },
-  {
-    id: "social-media-3",
-    icon: twitter,
-    link: "https://www.twitter.com/",
-  },
-  {
-    id: "social-media-4",
-    icon: linkedin,
-    link: "https://www.linkedin.com/",
+    icon: github,
+    link: "https://github.com/0xDigimon/Intelligent-Mobile-Robot-IMR",
   },
 ];
